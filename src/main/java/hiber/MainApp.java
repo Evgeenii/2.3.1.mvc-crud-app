870ddc911e2ce4134d10cadf77625b754f05e9fc
Eugene Kazmin <varenkine@gmail.com>
2022-09-30 22:05:08 +0300

    spring_hibernate

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,51 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        user1.setCar(new Car("BMW", 320));
+        userService.add(user1);
+
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        user2.setCar(new Car("Audi", 100));
+        userService.add(user2);
+
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        user3.setCar(new Car("VAZ", 2108));
+        userService.add(user3);
+
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+        user4.setCar(new Car("VAZ", 2106));
+        userService.add(user4);
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Model = " + user.getCar().getModel());
+            System.out.println("Series = " + user.getCar().getSeries());
+            System.out.println();
+            User userByModelAndSeries = userService.getUserBy(user1.getCar().getModel(),
+                    user1.getCar().getSeries());
+            System.out.println(userByModelAndSeries);
+        }
+
+        context.close();
+    }
+}
